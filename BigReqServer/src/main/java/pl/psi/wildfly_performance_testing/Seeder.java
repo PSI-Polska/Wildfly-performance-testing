735060e356ad38a743ca59c239a8f504cb26f054
ftrela <ftrela@psi.pl>
2016-07-08 14:29:54 +0200

    swap E class with C class in calculateMedian method; change numberOfObjects in Seeder; change datasource into PostgreSQLDS

--- BigReqServer/src/main/java/pl/psi/wildfly_performance_testing/Seeder.java
+++ BigReqServer/src/main/java/pl/psi/wildfly_performance_testing/Seeder.java
@@ -19,7 +19,7 @@ import java.util.logging.Logger;
 @Singleton
 @Startup
 public class Seeder {
-    private final int numberOfObjects = 150;
+    private final int numberOfObjects = 300;
     private final int creatingObjectProbabilityInPercentage = 50;
     private Random random = new Random(94);
 
